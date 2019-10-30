5692f1b1e72328c75c0ffda3f1e856e6edebd93b
evanandrewson <evanandrewson@gmail.com>
2019-10-30 15:12:38 -0700

    made test for App and ColorDisplay components

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
 
 describe('App component', () => {
   it('renders App', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = mount(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
