5aa3ef61fa6e50ff2b71928225b5bffcc1ca7c1d
evanandrewson <evanandrewson@gmail.com>
2019-10-30 14:08:15 -0700

    refactored color picker to have container and state separate from app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ColorContainer from '../containers/ColorContainer';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <ColorContainer/>
+      </>
+    )
+  }
+}
